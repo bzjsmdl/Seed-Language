d0bfe25bd9991fbaf614258d3037f2e7667b27fe
bzjsmdl <168650981+bzjsmdl@users.noreply.github.com>
2026-04-26 22:28:22 +0800

    Add files via upload

---
+++ src/main/state64.asm
@@ -0,0 +1,109 @@
+section .data
+	s db "%s"
+	cf db " %c"
+	c db 0
+	Character equ 0
+	Delimter equ 1
+	Quote equ 2
+	Number equ 3
+	NotCharacter equ 4
+	Space equ 5
+section .bss
+	stateArr resb 0x20000000
+section .text
+	global state
+	extern Isalpha, Isnum, Ispunct
+	extern scanf, printf
+	extern err, OK, BFW, UN
+	; char* state(unsigned long long int length, char* text)
+	state:
+		push rsi
+		push rdi
+		push rbx
+		cmp rcx, 0x20000000
+		jg .ask
+		mov rsi, rdx
+		mov rdi, stateArr
+		xor rbx, rbx
+		xor rdx, rdx
+		.mainloop:
+			lea rdx, byte [rsi + rbx]
+            push rcx
+			mov rcx, rdx
+			call Isalpha
+			test rax, rax
+			jne .c
+			mov rcx, rdx
+			call Isnum
+			test rax, rax
+			jne .n
+			cmp byte [rdx], "'"
+			je .q
+			cmp byte [rdx], '"'
+			je .q
+			mov rcx, rdx
+			call Ispunct
+			test rax, rax
+			jne .d
+			cmp byte [rdx], ' '
+			je .s
+			mov byte [rdi + rbx], NotCharacter
+		.next:
+            pop rcx
+			inc rbx
+			loop .mainloop
+		mov rax, rdi
+		.return:
+			pop rbx
+			pop rdi
+			pop rsi
+			ret
+		.c:
+			mov byte [rdi + rbx], Character
+			jmp .next
+		.n:
+			mov byte [rdi + rbx], Number
+			jmp .next
+		.q:
+			mov byte [rdi + rbx], Quote
+			jmp .next
+		.d:
+			mov byte [rdi + rbx], Delimter
+			jmp .next
+		.s:
+			mov byte [rdi + rbx], Space
+			jmp .next
+		.bfw:
+			mov dword [err], 6 ; 6 = BigFileWarnning
+		.err:
+			xor rax, rax
+			jmp .return
+		.ask:
+            push rcx
+            sub rsp, 32
+            mov rcx, s
+			mov rdx, BFW
+			call printf
+            add rsp, 32
+            pop rcx
+			.l:
+                sub rsp, 40
+				mov rdx, c
+				mov rcx, cf
+				call scanf
+                add rsp, 40
+                sub rsp, 40
+				mov rdx, OK
+				mov rcx, s
+				call printf
+				add rsp, 40
+				cmp byte [c], 'y'
+				je .mainloop
+				cmp byte [c], 'n'
+				je .bfw
+                sub rsp, 40
+				mov rdx, UN
+				mov rcx, s
+				call printf
+				add rsp, 40
+				jmp .l
